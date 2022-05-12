33906c24c285fe8df48088439f0befa18f1ba4b2
unknown <surendhrakumar.t@gmail.com>
2022-05-12 15:02:49 +0530

    Done Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-
+import { RouterModule, Routes, ExtraOptions } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileModule } from './profile/profile.module';
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+const routes: Routes = [
+{
+path: '',
+component: ProfileComponent
+}
+];
+const config: ExtraOptions = {
+useHash: true,
+};
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent
+],
+imports: [
+BrowserModule,
+FormsModule,
+ReactiveFormsModule,
+RouterModule.forRoot(routes, config),
+ProfileModule,
+HttpClientModule
+],
+providers: [],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
